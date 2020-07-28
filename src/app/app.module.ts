86b79fb1c07cbca524b7d332127a39c47cd2d48e
Elena Borodina <eborodina@verenia.com>
2020-07-28 09:36:26 +0300

    build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,6 @@ import { AppComponent } from './app.component';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestore } from 'angularfire2/firestore';
-// for AngularFireDatabase
-// import { AngularFireDatabaseModule } from 'angularfire2/database';
-// import { AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
-// for AngularFireAuth
-import { AngularFireAuthModule } from 'angularfire2/auth';
-import { AngularFireAuth } from 'angularfire2/auth';
 import { environment } from 'src/environments/environment';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';

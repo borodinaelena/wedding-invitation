b1b29f351bf3934b02fc9e81ed643175b26ec2a4
Elena Borodina <eborodina@verenia.com>
2020-07-30 11:45:01 +0300

    add roles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/I7CfaDYzTVM" frameborder="0"
-      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-    <router-outlet></router-outlet>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -7,34 +7,28 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
+  // template:`heloworld`,
   styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent {
   private familiesCollection: AngularFirestoreCollection<any>;
-  private familyMembersCollection: AngularFirestoreCollection<any>;
   public families: Observable<any[]>;
-  public familyMembers: Observable<any[]>;
   public allFamilies: any[] = [];
+  private id:string;
+  public family:any;
   constructor(
     db: AngularFirestore,
     private route: ActivatedRoute) {
     console.log('this.route.queryParams', this.route.snapshot.params.id);
+    this.id= this.route.snapshot.params.id;
 
     this.familiesCollection = db.collection<any>('family');
-    this.familyMembersCollection = db.collection<any>('family_member');
     this.families = this.familiesCollection.valueChanges();
-    this.familyMembers = this.familyMembersCollection.valueChanges();
 
-    this.families.subscribe(f => {
-      this.familyMembers.subscribe(m => {
-        f.map(family => {
-          family.membersInfo = [];
-          family.members.map((member, i) => {
-            family.membersInfo.push(m.find(element => element.id === member));
-          });
-        });
-        this.allFamilies = f;
-      });
+    this.families.subscribe(res => {
+        this.allFamilies = res;
+        this.family= this.allFamilies.find(f=>f.id===this.id);
     });
+
   }
 }
